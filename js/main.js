8d1540b150815e64e89303f6eb324ae3f5a6964f
MrLiono21 <lionelosamba@gmail.com>
2021-04-22 00:09:18 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,38 @@
+const img = document.getElementById('img');
+const button = document.getElementById('submitbtn');
+const input = document.getElementById('img_url');
+const result = document.getElementById('prediction');
+
+
+let model;
+
+button.onclick = () => {
+    const url = input.value;
+    img.src = url;
+    result.innerText = "Loading...";
+}
+
+img.onload = () => {
+   doPrediction();
+}
+
+function doPrediction() {
+
+    if ( model ) {
+        model.classify(img).then(predictions => {
+            showPrediction(predictions);
+            });
+    } else {
+        mobilenet.load().then(_model => {
+            model = _model;
+            model.classify(img).then(predictions => {
+                showPrediction(predictions);
+            });
+        });
+    }
+}
+
+function showPrediction(predictions) {
+    result.innerText = "This might be a " + predictions[0].className;
+
+}
